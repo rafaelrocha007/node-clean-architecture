2069cd2ae9d5677de521840ff772b730ed6040f5
Rafael Rocha <rafael.rocha.mg@gmail.com>
2020-12-18 13:50:02 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   // coverageProvider: "v8",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
