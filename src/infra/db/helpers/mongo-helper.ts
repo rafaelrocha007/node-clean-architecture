10f9f108f9536b21ae514c8701ef3e991504e78a
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-01-15 17:44:36 -0300

    feat: ensure mongodb reconnect if connection is down

--- src/infra/db/helpers/mongo-helper.ts
+++ src/infra/db/helpers/mongo-helper.ts
@@ -2,9 +2,11 @@ import { MongoClient, Collection } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
-  async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(url, {
+  async connect (uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
@@ -14,7 +16,10 @@ export const MongoHelper = {
     this.client.close()
   },
 
-  getCollection (name: string): Collection {
+  async getCollection (name: string): Promise<Collection> {
+    if (!this.client?.isConnected()) {
+      await this.connect(this.uri)
+    }
     return this.client.db().collection(name)
   },
 
