6bc1526c96b5d20d07c53b6fd2e771e6a8fdd4a4
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-01-14 23:41:06 -0300

    chore: remove code coverage from src/main folder

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { adaptRoute } from '../adapters/ExpressRouteAdapter'
 import { makeSignUpController } from '../factories/signup'
 
-export default (router: Router): any => {
+export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
