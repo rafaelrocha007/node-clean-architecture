d2f3c9b823c40ba3f5ac7056544037c297606496
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-01-14 23:06:32 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const salt = 12
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const addAccountRepository = new AccountMongoRepository()
+  const addAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
+  return new SignUpController(emailValidatorAdapter, addAccount)
+}
