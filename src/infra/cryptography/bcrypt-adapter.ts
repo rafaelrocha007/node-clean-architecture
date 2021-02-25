87778dfad86f297ce902e001bed568e5265f9c9c
Rafael Rocha <rafaelcosta@cloudfox.net>
2021-02-25 09:05:41 -0300

    fix: correcting compare return

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -16,6 +16,6 @@ export class BcryptAdapter implements Hasher, HashComparer {
 
   async compare (value: string, hash: string): Promise<boolean> {
     const isValid = await bcrypt.compare(value, hash)
-    return await new Promise(resolve => resolve(isValid))
+    return isValid
   }
 }
