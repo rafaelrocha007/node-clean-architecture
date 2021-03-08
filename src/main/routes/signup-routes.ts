ba1559ba3868bd0c2a0105cb1b955bf623be4cf8
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-03-08 09:41:29 -0300

    refactor: improve file names

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/ExpressRouteAdapter'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
