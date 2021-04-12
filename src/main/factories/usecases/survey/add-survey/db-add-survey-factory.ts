15018193b406c8a4598ba17fd192d6aa67dd0737
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-04-12 07:52:02 -0300

    feat: ensure load surveys route returns 403 if no accessToken is providade

--- src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
+++ src/main/factories/usecases/survey/add-survey/db-add-survey-factory.ts
@@ -2,7 +2,7 @@ import { AddSurvey } from '../../../../../domain/usecases/add-survey'
 import { SurveyMongoRepository } from '../../../../../infra/db/mongodb/survey/survey-mongo-repository'
 import { DbAddSurvey } from '../../../../../data/usecases/add-survey/db-add-survey'
 
-export const makeDbAddSurvery = (): AddSurvey => {
+export const makeDbAddSurvey = (): AddSurvey => {
   const surveyMongoRepository = new SurveyMongoRepository()
   return new DbAddSurvey(surveyMongoRepository)
 }
