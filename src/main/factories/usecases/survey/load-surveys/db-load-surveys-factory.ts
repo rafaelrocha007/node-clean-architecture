2b97cbebc5f81c7656dcd355a1321004d51825c2
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-04-20 08:04:41 -0300

    feat: ensure save survey result route returns 403 if no token is provided

--- src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
+++ src/main/factories/usecases/survey/load-surveys/db-load-surveys-factory.ts
@@ -1,7 +1,8 @@
 import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository'
 import { DbLoadSurveys } from '@/data/usecases/survey/load-surveys/db-load-surveys'
+import { LoadSurveys } from '@/domain/usecases/survey/load-surveys'
 
-export const makeDbLoadSurveys = (): DbLoadSurveys => {
+export const makeDbLoadSurveys = (): LoadSurveys => {
   const surveyMongoRepository = new SurveyMongoRepository()
   return new DbLoadSurveys(surveyMongoRepository)
 }
---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { auth } from '@/main/middlewares/auth'
+
+export default (router: Router): void => {
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}
