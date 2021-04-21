cd788ef3ee0c73f7edfaa86cfe526080a9a502b6
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-04-21 16:31:39 -0300

    refactor: change Promises to use short version

--- src/presentation/test/mock-survey.ts
+++ src/presentation/test/mock-survey.ts
@@ -7,7 +7,7 @@ import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id'
 export const mockLoadSurveys = (): LoadSurveys => {
   class LoadSurveysStub implements LoadSurveys {
     async load (): Promise<SurveyModel[]> {
-      return await new Promise(resolve => resolve(mockSurveyModels()))
+      return await Promise.resolve(mockSurveyModels())
     }
   }
   return new LoadSurveysStub()
@@ -16,7 +16,7 @@ export const mockLoadSurveys = (): LoadSurveys => {
 export const mockAddSurvey = (): AddSurvey => {
   class AddSurveyStub implements AddSurvey {
     async add (data: AddSurveyParams): Promise<void> {
-      return await new Promise(resolve => resolve())
+      return await Promise.resolve()
     }
   }
   return new AddSurveyStub()
@@ -25,7 +25,7 @@ export const mockAddSurvey = (): AddSurvey => {
 export const mockLoadSurveyById = (): LoadSurveyById => {
   class LoadSurveyByIdStub implements LoadSurveyById {
     async loadById (id: string): Promise<SurveyModel> {
-      return await new Promise(resolve => resolve(mockSurveyModel()))
+      return await Promise.resolve(mockSurveyModel())
     }
   }
 
