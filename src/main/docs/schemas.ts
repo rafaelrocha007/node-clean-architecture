3bcef983ada8d97eb475c13c287bdd61bdb3695d
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-04-29 07:16:15 -0300

    docs: update save survey result docs

--- src/main/docs/schemas.ts
+++ src/main/docs/schemas.ts
@@ -8,7 +8,8 @@ import {
   surveySchema,
   surveyAnswerSchema,
   surveysSchema,
-  surveyResultSchema
+  surveyResultSchema,
+  surveyResultAnswerSchema
 } from './schemas/'
 
 export default {
@@ -21,5 +22,6 @@ export default {
   survey: surveySchema,
   surveyAnswer: surveyAnswerSchema,
   surveyResult: surveyResultSchema,
+  surveyResultAnswer: surveyResultAnswerSchema,
   surveys: surveysSchema
 }
